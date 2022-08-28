184dd4fefca9095243c965e7db5696b720ec1e4e
abdoshbr3322 <abdullahelshebrawy@gmail.com>
2022-08-28 15:35:16 +0200

    Created banner section

--- js/main.js
+++ js/main.js
@@ -38,3 +38,18 @@ function showScrollToTopBtn() {
 function hideScrollToTopBtn() {
   btnscrolltoTop.classList.remove("active");
 }
+
+// Abdullah Elshebrawy: create banner section and carousel
+
+$(document).ready(function(){
+  $(".banner .owl-carousel").owlCarousel({
+    loop: true,
+    items: 1,
+    margin: 20,
+    center: true,
+    mouseDrag: true,
+    touchDrag: true,
+    dots: true,
+    dotsContainer: ".banner .bullets",
+  });
+});
